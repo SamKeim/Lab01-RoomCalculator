db8e4c98ab0686292e9b1bba78a2d5efb32521f7
Sam Keim <skeim235@gmail.com>
2020-01-22 10:08:50 -0500

    Fixed final error loop and added extra commenting

--- src/RoomCalculator.java
+++ src/RoomCalculator.java
@@ -1,7 +1,7 @@
 
 /*
  * 
- * @Author Sam Keim -  GitHub @ Samtheenby
+ *  @Author Sam Keim -  GitHub @ Samtheenby
  */
 
 import java.util.Scanner;
@@ -11,41 +11,49 @@ public class RoomCalculator {
 	public static void main(String[] args) {
 		
 		// Room Calculator
+		// Declare Variables
 		Scanner scnr = new Scanner(System.in);
 		final double HEIGHT = 10.0;
 		System.out.println("Welcome to Grand Circus' Room Detail Generator!");
 		System.out.println("Would you like to use the calculator? (Y/N)");
-		String userChoice = scnr.nextLine();
+		String userChoice = scnr.next();
 		
+		// Check for breakage
 		while (!(userChoice.substring(0,1).equalsIgnoreCase("Y")) && !(userChoice.substring(0,1).equalsIgnoreCase("N"))) {
 			System.out.println("Please enter Y or N.");
-			userChoice = scnr.nextLine();
+			userChoice = scnr.next();
 		}
+		
+		// While loop for while answer begins with "Y"
 		while (userChoice.substring(0,1).equalsIgnoreCase("Y")) {
+			// Get user input
 			System.out.println("Enter Length:");
 			double length = scnr.nextDouble();
-			
 			System.out.println("Enter Width:");
 			double width = scnr.nextDouble();
 			
+			// Calculations
 			double area = length * width;
 			double perimeter = (length + width) * 2;
 			double volume = length * width * HEIGHT;
 			
+			// Print results
 			System.out.println("Area: " + area);
 			System.out.println("Perimeter: " + perimeter);
 			System.out.println("Volume: " + volume);
 			
+			// Ask to continue
 			System.out.println("Would you like to calculate another room? (Y/N)");
-			userChoice = scnr.nextLine();
+			userChoice = scnr.next();
 			
-			//I would like to get this to work with the .substring.equalsIgnoreCase but it is not working and I am out of time for problem solving
-			while (!(userChoice.equalsIgnoreCase("Y")) && !(userChoice.equalsIgnoreCase("N"))) {
-			System.out.println("Please enter Y or N.");
-			userChoice = scnr.nextLine();
-			
+			//Check for breakage
+			while (!(userChoice.substring(0,1).equalsIgnoreCase("Y")) && !(userChoice.substring(0,1).equalsIgnoreCase("N"))) {
+				System.out.println("Please enter Y or N.");
+				userChoice = scnr.next();
 			}
 		}
+		
+		//Exit protocol
 		System.out.println("Thank you, goodbye.");
 		scnr.close();
 	}
